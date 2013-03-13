2d9894ff0c312bed6fec65b6ee5dca7fa14e68b7
Richard Pougnet <richard@pougnet.ca>
2013-03-12 21:30:03 -0400

    Working command parsing with install and help parameters

--- uaurlazy.py
+++ uaurlazy.py
@@ -5,16 +5,25 @@ import os
 import urllib
 import subprocess
 
-url = "https://aur.archlinux.org/packages/" + sys.argv[1][0] + sys.argv[1][1] + "/" + sys.argv[1] + "/PKGBUILD"
+if sys.argv[1] == "help":
+	print "Usage:"
+	print "Parameters:"
+	print "		install packagename - installs the package"
+	print "		help - displays this useful help file"	
+elif sys.argv[1] == "install":
+	url = "https://aur.archlinux.org/packages/" + sys.argv[2][0] + sys.argv[2][1] + "/" + sys.argv[2] + "/PKGBUILD"
 
-print "Changing directory to /tmp for build process"
-os.chdir("/tmp/")
+	print "Changing directory to /tmp for build process"
+	os.chdir("/tmp/")
 
-print "Downloading %s from %s" % (sys.argv[1], url)
-urllib.urlretrieve(url, "PKGBUILD")
+	print "Downloading %s from %s" % (sys.argv[2], url)
+	urllib.urlretrieve(url, "PKGBUILD")
 
-print "Building and installaing %s" % sys.argv[1]
-subprocess.call(["makepkg", "-sic"])
+	print "Building and installaing %s" % sys.argv[2]
+	subprocess.call(["makepkg", "-sic"])
 
-print "Cleaning up"
-subprocess.call(["rm", "PKGBUILD"])
+	print "Cleaning up"
+	subprocess.call(["rm", "PKGBUILD"])
+else:
+	print "Invalid option, for usage please enter 'uaurlazy help'"
+	os.sysexit()
